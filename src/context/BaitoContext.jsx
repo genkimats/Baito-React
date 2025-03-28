63ccde240ecc16c18bae94aef3550a0cb5ed50b8
genkimats <genkimatsu24@gmail.com>
2025-03-28 18:50:38 +0900

    implement select time and calendar workday highlight

--- src/context/BaitoContext.jsx
+++ src/context/BaitoContext.jsx
@@ -5,6 +5,15 @@ const BaitoContext = createContext();
 export const useBaitoContext = () => useContext(BaitoContext);
 
 export const BaitoManager = ({ children }) => {
+  // Configs
+  const DEFAULT_START_HOUR = 17;
+  const DEFAULT_START_MINUTE = 0;
+  const DEFAULT_END_HOUR = 22;
+  const DEFAULT_END_MINUTE = 0;
+
+  const PAY_INTERVAL_MINUTES = 15;
+
+  // Managing workdays
   const [savedDate, setSavedDate] = useState(new Date());
   const yearMonth = `${savedDate.getFullYear()}-${String(
     savedDate.getMonth() + 1
@@ -38,6 +47,11 @@ export const BaitoManager = ({ children }) => {
   };
 
   const value = {
+    DEFAULT_START_HOUR,
+    DEFAULT_START_MINUTE,
+    DEFAULT_END_HOUR,
+    DEFAULT_END_MINUTE,
+    PAY_INTERVAL_MINUTES,
     workdays,
     setWorkdays,
     savedDate,
--- src/pages/ManageWorkdayPage.jsx
+++ src/pages/ManageWorkdayPage.jsx
@@ -1,12 +1,24 @@
 import Calendar from "react-calendar";
 import "../css/Calendar.css";
 import { useEffect, useState } from "react";
-import { Switch, Typography, Button } from "@mui/material";
+import {
+  Switch,
+  Typography,
+  Button,
+  TextField,
+  MenuItem,
+  InputLabel,
+} from "@mui/material";
 import "../css/ManageWorkdayPage.css";
 import { useBaitoContext } from "../context/BaitoContext";
 
 function ManageWorkdayPage() {
   const {
+    DEFAULT_START_HOUR,
+    DEFAULT_START_MINUTE,
+    DEFAULT_END_HOUR,
+    DEFAULT_END_MINUTE,
+    PAY_INTERVAL_MINUTES,
     workdays,
     setWorkdays,
     savedDate,
@@ -19,9 +31,51 @@ function ManageWorkdayPage() {
   const [selectedDay, setSelectedDay] = useState(initialDate.getDate());
   const [isAddMode, setIsAddMode] = useState(true); // true -> Add , false -> Remove
 
+  const [startTime, setStartTime] = useState({
+    hour: DEFAULT_START_HOUR,
+    minute: DEFAULT_START_MINUTE,
+  });
+  const [endTime, setEndTime] = useState({
+    hour: DEFAULT_END_HOUR,
+    minute: DEFAULT_END_MINUTE,
+  });
+
+  let calendarDate = new Date(
+    savedDate.getFullYear(),
+    savedDate.getMonth(),
+    selectedDay
+  );
+
+  console.log(workdays);
+
+  const hourOptions = Array.from({ length: 24 }, (_, i) => i);
+  const minuteOptions = Array.from(
+    { length: 60 / PAY_INTERVAL_MINUTES },
+    (_, i) => i * PAY_INTERVAL_MINUTES
+  );
+
   const handleDayClick = (date) => {
-    setSelectedDay(date);
+    setSelectedDay(date.getDate());
     console.log(selectedDay);
+  };
+
+  const handleTimeChange = (timeType, field) => (e) => {
+    e.preventDefault();
+    const value = parseInt(e.target.value);
+
+    if (timeType === "start") {
+      if (field === "hour") {
+        setStartTime((prev) => ({ ...prev, [field]: value }));
+      } else {
+        setStartTime((prev) => ({ ...prev, [field]: value }));
+      }
+    } else {
+      if (field === "hour") {
+        setEndTime((prev) => ({ ...prev, [field]: value }));
+      } else {
+        setEndTime((prev) => ({ ...prev, [field]: value }));
+      }
+    }
   };
 
   const handleToggle = (event) => {
@@ -29,9 +83,17 @@ function ManageWorkdayPage() {
   };
   const handleAdd = (e) => {
     e.preventDefault();
-    alert("Added!");
 
-    addWorkday({});
+    if (!workdays.some((workday) => workday.day === selectedDay)) {
+      addWorkday({
+        day: parseInt(selectedDay),
+        startTime: startTime,
+        endTime: endTime,
+      });
+      alert("Added!");
+    } else {
+      alert("Duplicate!");
+    }
   };
 
   const handleRemove = (e) => {
@@ -40,6 +102,7 @@ function ManageWorkdayPage() {
   };
 
   const handleConfirm = (e) => {
+    e.preventDefault();
     if (isAddMode) {
       handleAdd(e);
     } else {
@@ -71,16 +134,86 @@ function ManageWorkdayPage() {
       </div>
       <Calendar
         className={"react-calendar"}
-        value={
-          new Date(savedDate.getFullYear(), savedDate.getMonth(), selectedDay)
-        }
+        value={calendarDate}
         onClickDay={handleDayClick}
         view="month"
         showNeighboringMonth={false}
+        tileClassName={({ date }) =>
+          workdays.some((workday) => workday.day === date.getDate())
+            ? "workday"
+            : ""
+        }
       ></Calendar>
 
       <div className="select-time">
-        {isAddMode ? <p>Select time here</p> : <></>}
+        {isAddMode ? (
+          <>
+            <div className="start-time">
+              <TextField
+                select
+                value={startTime.hour}
+                onChange={handleTimeChange("start", "hour")}
+                label="Hours"
+                size="small"
+              >
+                {hourOptions.map((hour) => (
+                  <MenuItem key={`start-h-${hour}`} value={hour}>
+                    {String(hour).padStart(2, "0")}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                select
+                value={startTime.minute}
+                onChange={handleTimeChange("start", "minute")}
+                label="Minutes"
+                size="small"
+                type="number"
+                slotProps={{
+                  InputLabel: {
+                    shrink: true,
+                  },
+                }}
+              >
+                {minuteOptions.map((minute) => (
+                  <MenuItem key={`start-h-${minute}`} value={minute}>
+                    {String(minute).padStart(2, "0")}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </div>
+            <div className="end-time">
+              <TextField
+                select
+                value={endTime.hour}
+                onChange={handleTimeChange("end", "hour")}
+                label="Hours"
+                size="small"
+              >
+                {hourOptions.map((hour) => (
+                  <MenuItem key={`end-h-${hour}`} value={hour}>
+                    {String(hour).padStart(2, "0")}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                select
+                value={endTime.minute}
+                onChange={handleTimeChange("end", "minute")}
+                label="Minutes"
+                size="small"
+              >
+                {minuteOptions.map((minute) => (
+                  <MenuItem key={`end-h-${minute}`} value={minute}>
+                    {String(minute).padStart(2, "0")}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
 
       <Button variant="contained" onClick={handleConfirm}>
